b8058e487eb44e73d3e60fac76262640fdce6786
priyesh1630 <patil998707@gmail.com>
2025-11-01 00:47:30 +0530

    page object module

---
+++ src/test/java/SeleniumPractice/StandaloneTest.java
@@ -0,0 +1,15 @@
+package SeleniumPractice;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StandaloneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
+		driver.manage().window().fullscreen();
+	}
+
+}
