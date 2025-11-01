a0247d3bbcc2206b374fa9cda80c8fe992694a35
yash <patil998707@gmail.com>
2025-11-01 12:23:20 +0530

    implement the  page  oject model  and pagefactory in the  code

--- src/main/java/Practice/LandingPage.java
+++ src/main/java/Practice/LandingPage.java
@@ -3,6 +3,8 @@ package Practice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LandingPage {
 	
@@ -10,8 +12,36 @@ public class LandingPage {
 	
 	public LandingPage(WebDriver driver) {
 		this.driver=driver;
+		//in the constructor  itself 
+		//this  will trigger initlizing all the elements
+		//this method  expected two arguments .one you have to send  driver and  another  argument is like  this refers to the current class driver
+		PageFactory.initElements(driver, this);
 	}
 	
-	WebElement userEmail = driver.findElement(By.id("userEmail"));
+	//WebElement userEmail = driver.findElement(By.id("userEmail"));
+	//this  is  page  factory design pattern
+	@FindBy(id="userEmail")
+	WebElement uaseremail;
 	
-}
+	@FindBy(id="userPassword")
+	WebElement userpassword;
+	
+	@FindBy(id="login")
+	WebElement submit;
+	
+	
+	public void loginApplication(String email,String password) {
+		uaseremail.sendKeys(email);
+		userpassword.sendKeys(password);
+		submit.click();
+	}
+
+
+	public void goTo() {
+		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
+	}
+		
+	}
+
+
+
--- src/test/java/SeleniumPractice/StandaloneTest.java
+++ src/test/java/SeleniumPractice/StandaloneTest.java
@@ -2,14 +2,20 @@ package SeleniumPractice;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import Practice.LandingPage;
+
 public class StandaloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
-		driver.manage().window().fullscreen();
+		//driver.get("https://rahulshettyacademy.com/client/#/auth/login");
+		driver.manage().window().maximize();
+		
+		LandingPage LandingPage = new LandingPage(driver);
+		LandingPage.goTo();
+		LandingPage.loginApplication("yash0207@gmail.com", "Yash@123");
 	}
 
 }
