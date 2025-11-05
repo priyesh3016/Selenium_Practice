eec152f45dfa7a500198cf6db075501c0344d956
yash <patil998707@gmail.com>
2025-11-05 10:39:45 +0530

    add link tag details

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -1,5 +1,44 @@
 package TestComponents;
 
-public class BaseTest {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+	
+	public WebDriver driver;
+
+	public void initializeDriver() throws IOException {
+		//In Selenium, Global Properties (also known as Global Variables or Configuration Properties) are values that are used across the entire test framework
+		//how  to setup global  properties 
+	//properties  class  read  the  global  properties 
+		
+	
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"C:\\Users\\Admin\\Desktop\\GitDemo\\Selenium_Practice\\src\\test\\java\\Resources\\GlobalData.properties");
+		prop.load(fis);
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equalsIgnoreCase("chrome"))
+		{
+			ChromeDriver driver = new ChromeDriver();
+		
+		}else if(browserName.equalsIgnoreCase("firefox")) {
+			
+			//firefox
+			
+		}else if(browserName.equalsIgnoreCase("edge")) {
+			//edge
+			
+		}	
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+	}
+	
+	
+	
 }
