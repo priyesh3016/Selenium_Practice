d4d58411d1c025cb01b0d28d8647d9bede4f9c7b
yash <patil998707@gmail.com>
2026-07-13 18:13:14 +0530

    webtable practice code

--- src/main/java/WebTable/WebTable.java
+++ src/main/java/WebTable/WebTable.java
@@ -1,5 +1,7 @@
 package WebTable;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebTable {
@@ -10,8 +12,32 @@ public class WebTable {
 		driver.manage().window().maximize();
 		driver.get("https://demoqa.com/webtables");
 		
-		//Step 1 : 
+		//Step 1 : CLick on add  button 
+		
+		WebElement addButton = driver.findElement(By.id("addNewRecordButton"));
+				addButton.click();
+				
+		//Step 2 : registration  Form details 
+				
+				driver.findElement(By.id("firstName")).sendKeys("Priyesh");
+				driver.findElement(By.id("lastName")).sendKeys("Patil");
+				driver.findElement(By.id("userEmail")).sendKeys("sensys348@gmail.com");
+				driver.findElement(By.id("age")).sendKeys("27");
+				driver.findElement(By.id("salary")).sendKeys("5855");
+				driver.findElement(By.id("department")).sendKeys("QA");
+				driver.findElement(By.id("submit")).click();	
+				
+		// Step 3 : Validate  
+				
+				String result = driver.findElement(By.cssSelector("tbody tr:nth-child(4)")).getText();
+				
+				if(result.contains("Priyesh")) {
+					System.out.println("Test Status: PASSED");
+			   	} else {
+					System.out.println("Test Status: FAILED - Selection text missing");	
+					}
+				}
 		
 	}
 
-}
+
